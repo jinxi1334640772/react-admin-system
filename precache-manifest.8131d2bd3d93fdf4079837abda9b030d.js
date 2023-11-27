a208a4883da6a77fcbc0024734047866214b0112
zhangjinxi <1334640772@qq.com>
2023-11-28 07:41:22 +0800

    deploy

---
+++ precache-manifest.8131d2bd3d93fdf4079837abda9b030d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9086280f1920c42ee2f0deb69561527c",
+    "url": "/react-admin-systemindex.html"
+  },
+  {
+    "revision": "a48a256158e5b67a4c23",
+    "url": "/react-admin-systemstatic/css/2.29d328ae.chunk.css"
+  },
+  {
+    "revision": "a48a256158e5b67a4c23",
+    "url": "/react-admin-systemstatic/js/2.b9dbcafa.chunk.js"
+  },
+  {
+    "revision": "e01039c34898d2a611de789378dea73a",
+    "url": "/react-admin-systemstatic/js/2.b9dbcafa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2e6212db41a8e04ec27",
+    "url": "/react-admin-systemstatic/js/main.7081e796.chunk.js"
+  },
+  {
+    "revision": "b0e6305742161acd001f",
+    "url": "/react-admin-systemstatic/js/runtime-main.0cbbcdfc.js"
+  }
+]);
